f21df5d8c00230b8899a085308d799cb0066e2e5
cgmondejar <mondejar.chrisvem1989@gmail.com>
2026-03-20 09:16:18 +0000

    Update WN11-SO-000205.ps1

--- STIG-Implementation/WN11-SO-000205.ps1
+++ STIG-Implementation/WN11-SO-000205.ps1
@@ -1,3 +1,29 @@
+<#
+.SYNOPSIS
+    This PowerShell script configures LmCompatibilityLevel to 5, enforcing NTLMv2 authentication 
+    only and disabling the insecure LM and NTLMv1 protocols.
+
+.NOTES
+    Author          : Chrisvem Mondejar
+    LinkedIn        : linkedin.com/in/cgmondejar/
+    GitHub          : github.com/cgmondejar
+    Date Created    : 2026-03-20
+    Last Modified   : 2026-03-20
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-SO-000205
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Copy, paste, and run the script. No further instructions required. 
+#>
+
 $keyPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa"
 
 # Create the key if it doesn't exist (extremely unlikely, but safe)
