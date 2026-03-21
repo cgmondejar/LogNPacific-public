bdf20d6459954c57dcb355abbf9ad361e797d211
cgmondejar <mondejar.chrisvem1989@gmail.com>
2026-03-21 09:02:11 +0000

    Update remediation-Require SMB Signing.ps1

--- automation/SMB+VerifyTrust/remediation-Require SMB Signing.ps1
+++ automation/SMB+VerifyTrust/remediation-Require SMB Signing.ps1
@@ -1,3 +1,29 @@
+<#
+.SYNOPSIS
+    Enforces SMB signing on both the client and server by requiring and enabling security 
+    signatures, helping prevent man-in-the-middle (MITM) and relay attacks on SMB communications.
+
+.NOTES
+    Author          : Chrisvem Mondejar
+    LinkedIn        : linkedin.com/in/cgmondejar/
+    GitHub          : github.com/cgmondejar
+    Date Created    : 2026-03-3
+    Last Modified   : 2026-03-3
+    Version         : 1.0
+    CVEs            : CVE-2025-33073
+    Plugin IDs      : 57608
+    STIG-ID         : N/A
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Copy, paste, and run the script. No further instructions required. 
+#>
+
 # Enforces SMB signing to prevent relay/MITM attacks
 
 Write-Host "Enforcing SMB signing..."
